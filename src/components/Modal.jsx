fe3284573bfc86d4488315b75aae311d0cecbe4f
Endriyani Rahayu <endriyani.rahayu@ui.ac.id>
2024-08-14 14:49:26 +0700

    updated class syntax to className

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,18 +4,18 @@ const Modal = ({ shouldShow, onRequestClose, children, title }) => {
   return (
     shouldShow && (
       <div
-        class="relative z-10"
+        className="relative z-10"
         aria-labelledby="modal-title"
         role="dialog"
         aria-modal="true"
       >
         <div
-          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
           aria-hidden="true"
         ></div>
 
-        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             {children}
           </div>
         </div>
