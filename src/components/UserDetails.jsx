eb2a9eac1bfbbd2ee5cdbf28f03904b70cdcd8dd
Endriyani Rahayu <endriyani.rahayu@ui.ac.id>
2024-08-14 18:36:43 +0700

    updated semantic

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -55,7 +55,7 @@ const UserDetails = () => {
           </p>
         </div>
         <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-          <dl className="sm:divide-y sm:divide-gray-200">
+          <div className="sm:divide-y sm:divide-gray-200">
             <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <div className="text-sm font-medium text-gray-500">Full name</div>
               <div className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
@@ -80,7 +80,7 @@ const UserDetails = () => {
                 {getBirthdayDateFormat(userData.birth_date)}
               </div>
             </div>
-          </dl>
+          </div>
         </div>
       </div>
     </div>
